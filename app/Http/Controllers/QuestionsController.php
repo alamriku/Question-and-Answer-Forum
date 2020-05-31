9f67191809f929d17735e2c55a49a8ea726014a9
Riku <alamriku069@gmail.com>
2020-06-01 01:05:36 +0600

    display all questins:

---
+++ app/Http/Controllers/QuestionsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+use Illuminate\Http\Request;
+
+class QuestionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $questions = Question::latest()->paginate(5);
+        return view('questions.index',compact('questions'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Questions  $questions
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $questions)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Questions  $questions
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Question $questions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Questions  $questions
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $questions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Questions  $questions
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $questions)
+    {
+        //
+    }
+}
