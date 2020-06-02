663de91dae2308bf9176e57123d3cae7ff1db09d
Riku <alamriku069@gmail.com>
2020-06-02 22:11:15 +0600

    add votes,answers,views counter to question item, new tech is scss used lesson-6

--- app/Question.php
+++ app/Question.php
@@ -29,4 +29,14 @@ class Question extends Model
         return $this->created_at->diffForHumans();
        // return $this->created_at->formate('d/m/Y');
     }
+
+    public function getStatusAttribute(){
+            if($this->answers > 0){
+                if($this->best_answer_id){
+                    return 'answered-accepted';
+                }
+                return 'answered';
+            }
+            return 'unanswered';
+    }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,6 +10,17 @@
                     <div class="card-body">
                         @foreach($questions as $question)
                             <div class="media">
+                                <div class="d-flex flex-column counters">
+                                    <div class="vote">
+                                        <strong>{{$question->votes}}</strong> {{\Illuminate\Support\Str::plural('vote',$question->votes)}}
+                                    </div>
+                                    <div class="status {{$question->status}}">
+                                        <strong>{{$question->answers}}</strong> {{\Illuminate\Support\Str::plural('answers',$question->votes)}}
+                                    </div>
+                                    <div class="view">
+                                        {{$question->views ." ".\Illuminate\Support\Str::plural('view',$question->view)}}
+                                    </div>
+                                </div>
                                 <div class="media-body">
                                     <h3 class="mt-0"><a href="{{$question->url}}">{{$question->title}}</a></h3>
                                       <p class="lead">
