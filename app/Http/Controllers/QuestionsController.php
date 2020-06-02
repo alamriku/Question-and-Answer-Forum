1452ade7ce7e0e8ff0abc55848839e6ac8e39e3e
Riku <alamriku069@gmail.com>
2020-06-02 22:36:40 +0600

    Build Questions Form

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -29,7 +29,9 @@ class QuestionsController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+
+        return view('questions.create',compact($question));
     }
 
     /**
